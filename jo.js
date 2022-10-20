12ffa56b3ea045ccf4f8536add95b37715d16695
Janzz-Botzz <114346949+Janzz-Botzz@users.noreply.github.com>
2022-10-20 15:26:52 +0700

    Update and rename jo .js to jo.js

--- jo.js
+++ jo.js
@@ -1,3 +1,4 @@
 https://www.mediafire.com/file/ynshtnemttbcnu9/jo.zip/file
 https://bit.ly/3axrco2
 https://www.mediafire.com/file/hnvbfoj3oh92vjp/jo-private.zip/file
+https://www.mediafire.com/file/0nh4u7qk29n0vhh/jo.zip/file
